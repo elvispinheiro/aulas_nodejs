9e5bb5abf8dc52760ecbe0ee6f295b7744daae5e
elvispinheiro <elvis.pinheiro1996@gmail.com>
2022-11-16 22:42:48 -0300

    nova_aula

--- routes/cliente.js
+++ routes/cliente.js
@@ -1,23 +1,28 @@
 const {Router}= require("express");
+// const { removeAllListeners } = require("nodemon");
+const { criar, atualizar, remover } = require("../controllers/cliente");
 const router = Router();
 
 router.get("/:id?", (req, res) => {
     res.send("lista de clientes");
 });
 
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.send("Criar cliente");
+router.post("/", async (req, res) => {
+    const result = await criar(req.body);
+
+    res.send(result);
 });
 
-router.put("/:id", (req, res) => {
-    console.log(req.body);
-    res.send("Atualizar cliente");
+router.put("/:id", async (req, res) => {
+    const result = await atualizar(req.params.id, req.body);     
+
+    res.send(result);
 });
 
-router.delete("/:id", (req, res) => {
-    console.log(req.body);
-    res.send("Remover Cliente");
+router.delete("/:id", async (req, res) => {
+    const result = await remover(req.params.id, req.body);
+
+    res.send();
 });
 
 module.exports = router;
